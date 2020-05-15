258e642e9c5bf9f140a8b5a1bc84c76319b3f950
XuWeidongCQ <1455049258@qq.com>
2020-05-15 20:42:39 +0800

    第一次提交

---
+++ src/components/LoginLayout/LoginForm.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="form-wrapper">
+        <div class="username">请输入用户名:</div>
+        <div>
+            <input type="text"  placeholder="用户名">
+        </div>
+        <div class="password">请输入密码:</div>
+        <div>
+            <input type="text" placeholder="密码">
+        </div>
+        <div class="btn-wrapper">
+           <input type="button" value="登录" @click="login">
+            <p>忘记密码？</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'LoginForm',
+    methods:{
+        login:function(){
+            this.$router.push('/home')
+        }
+    }
+}
+</script>
+
+<style scoped>
+.form-wrapper {
+    margin: 80px 50px;
+    background-color: rgba(255, 255, 255, 0.6);
+    border-radius: 20px;
+    padding: 40px 20px;
+}
+.password {
+    margin-top: 20px;
+}
+.form-wrapper input{
+    width: 100%;
+    box-sizing: border-box;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #cccccc;
+    font-size: 16px;
+}
+.btn-wrapper {
+    margin-top: 20px;
+}
+.btn-wrapper > input {
+    background-color: #29a745;
+    color: #ffffff;
+    font-size: 17px;
+    cursor: pointer;
+}
+.btn-wrapper > input:hover {
+    background-color: #269e40;
+}
+</style>
+
+
---
+++ src/views/LoginLayout.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="wrapper">
+        <div class="login-form-wrapper">
+            <div class="left">
+                <login></login>
+            </div>
+            <div class="right">
+                <login-form></login-form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Login from '@/components/LoginLayout/Logo.vue'
+import LoginForm from '@/components/LoginLayout/LoginForm.vue'
+export default {
+    components:{
+        Login,
+        LoginForm
+    }
+}
+</script>
+
+<style scoped>
+.wrapper {
+    background-image: url('../assets/login/background-small.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.login-form-wrapper {
+    min-width: 1366px;
+    height: 550px;
+    display: flex;
+    background-color:rgba(255, 255, 255, 0.4);
+    border-radius: 20px;
+    animation: 1s ease 0s moveIn;
+   
+}
+.login-form-wrapper > div {
+    flex: 1;
+    padding: 50px;
+}
+
+@keyframes moveIn {
+    from {
+        opacity: 0;
+        transform: translateY(-20px)
+    }
+    to {
+        opacity: 1;
+        transform: translateY(0)
+    }
+}
+</style>
